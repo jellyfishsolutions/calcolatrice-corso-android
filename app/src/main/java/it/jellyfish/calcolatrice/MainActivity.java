43e1ff6dc7869ce61793b90482dfae29caede630
Luca Roverelli <luca.roverelli@gmail.com>
2018-09-26 16:40:45 +0200

    Initial project

---
+++ app/src/main/java/it/jellyfish/calcolatrice/MainActivity.java
@@ -0,0 +1,13 @@
+package it.jellyfish.calcolatrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
